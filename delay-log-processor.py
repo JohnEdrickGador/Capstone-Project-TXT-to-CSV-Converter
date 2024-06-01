b0016e47e96cb2ef377a717c8957046972c7764d
JohnEdrickGador <jmgador@up.edu.ph>
2024-06-02 00:42:37 +0800

    fix: single column processing issue

--- delay-log-processor.py
+++ delay-log-processor.py
@@ -1,6 +1,20 @@
 import csv
 import os
 
+def csvMaker(entries, fileName, nodeNumber):
+    fields = entries[0]
+    csvFileName = f"{fileName}-Node-{nodeNumber}"
+    saveDestination = r"D:\Documents\School\UP_Diliman\Fourth-year\Second-Semester\CoE-199\CSV-Files"
+    directory = os.path.join(saveDestination,csvFileName)
+    with open(directory, mode = "w", newline='') as csvfile:
+        fieldNames = fields
+        writer = csv.DictWriter(csvfile, fieldnames=fieldNames)
+        writer.writeheader()
+
+        for entry in entries[1::]:
+            row = {fields[0]:entry[0]}
+            writer.writerow(row)
+
 def delayProcessor(filePath, fileName, columnCount, nodeNumber):
     file = open(filePath)
     entries = []
@@ -15,10 +29,10 @@ def delayProcessor(filePath, fileName, columnCount, nodeNumber):
                     entry[i] = float(entry[i])
                 except:
                     entry[i] = entry[i]
-    else:
+    elif columnCount == 1:
         for line in file:
             data = line.strip()
-            entries.append(data)
+            entries.append([data])
         for entry in entries[1::]:
             for i in range(1, columnCount):
                 try:
@@ -27,7 +41,7 @@ def delayProcessor(filePath, fileName, columnCount, nodeNumber):
                     entry[i] = entry[i]
     file.close()
 
-    
+    csvMaker(entries, fileName, nodeNumber)
     print(f"Finished generating {fileName}-Node-{nodeNumber}")
 
 def processDelayLog(filePath, nodeNumber):
